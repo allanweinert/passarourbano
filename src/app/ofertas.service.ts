4939d2521e71b2937ecddd75297af5af742f16ef
Allan Weinert <allan_weinert@hotmail.com>
2019-11-09 13:18:54 -0300

    Rotas

---
+++ src/app/ofertas.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
+import { Oferta } from './shared/oferta.module'
+
+@Injectable()
+export class OfertasService{
+    constructor(private http: HttpClient){}
+
+    public getOfertas(): Promise<Array<Oferta>>{
+        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+            .toPromise()
+            .then((resposta: any) => resposta)
+    }
+}
