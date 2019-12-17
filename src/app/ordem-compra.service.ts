6184aac964c409e959f8b3a94596eab1ffcfc861
Allan Weinert <allan_weinert@hotmail.com>
2019-12-17 00:06:51 -0300

    Injetando http e enviando form via http

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,9 +1,28 @@
-import { Pedido } from './shared/pedido.model'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { Http, RequestOptions, Headers, Response } from '@angular/http'
 
+import { Pedido } from './shared/pedido.model'
+import { URL_API } from './app.api'
+
+
+@Injectable()
 export class OrdemCompraService {
 
-  public efetivarCompra(pedido: Pedido): void {
-    console.log(pedido)
+  constructor(private http: Http) {}
+
+  public efetivarCompra(pedido: Pedido): Observable<any> {
+
+    let headers: Headers = new Headers()
+
+    headers.append('Content-type', 'application/json')
+
+    return this.http.post(
+      `${URL_API}/pedidos`,
+      JSON.stringify(pedido),
+      new RequestOptions({ headers: headers })
+    )
+    .map((resposta: Response) => console.log(resposta.json()) )
   }
 
 }
