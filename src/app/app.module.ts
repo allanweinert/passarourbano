d442f82b67ff67b6569ab342f951ed9383d0de73
Allan Weinert <allan_weinert@hotmail.com>
2019-12-17 00:07:19 -0300

    Criando componente ordem compra sucesso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,8 @@ import localePt from "@angular/common/locales/pt";
 
 //pipe
 import { DescricaoReduzida } from './util/descricao-reduzida.pipe';
-import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component'
+import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component';
+import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra-sucesso.component'
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component'
     ComoUsarComponent,
     OndeFicaComponent,
     DescricaoReduzida,
-    OrdemCompraComponent
+    OrdemCompraComponent,
+    OrdemCompraSucessoComponent
   ],
   imports: [
     BrowserModule,
